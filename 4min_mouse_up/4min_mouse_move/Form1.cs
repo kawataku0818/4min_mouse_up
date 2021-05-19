4587685b538d5385e2c83578f4f6d2e5d144c02d
kawataku0818 <oguri-san@hotmail.co.jp>
2021-05-19 09:06:44 +0900

    Add files via upload

---
+++ 4min_mouse_up/4min_mouse_move/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4min_mouse_move
+{
+
+    public partial class Form1 : Form
+    {
+        [DllImport("USER32.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTUP = 0x4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Cursor.Position = new Point(1, 1);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+    }
+}
